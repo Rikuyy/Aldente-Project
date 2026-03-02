b4a48fb3723ae7f0bdd563a63b186fecdc0914d3
RerendKurr <renistooshort@gmail.com>
2026-03-02 11:20:53 +0700

    Landasan dan Tampilan Login System

---
+++ lib/login_system/splash.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:cook_mate/login_system/sign_in.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const String routeName = '/splash';
+
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to CookMate, Time to cook!",
+      "image": "https://i.postimg.cc/mhhVywp9/splash-1.png"
+    },
+    {
+      "text": "We help people to find best recipe",
+      "image": "https://i.postimg.cc/3x3ZzL8C/splash-2.png"
+    },
+    {
+      "text": "We show the easy way to cook. Just stay at home with us",
+      "image": "https://i.postimg.cc/3x3ZzL8C/splash-2.png"
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: PageView.builder(
+                  onPageChanged: (value) {
+                    setState(() {
+                      currentPage = value;
+                    });
+                  },
+                  itemCount: splashData.length,
+                  itemBuilder: (context, index) => SplashContent(
+                    image: splashData[index]["image"],
+                    text: splashData[index]['text'],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: <Widget>[
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          splashData.length,
+                          (index) => AnimatedContainer(
+                            duration: const Duration(milliseconds: 250),
+                            margin: const EdgeInsets.only(right: 5),
+                            height: 6,
+                            width: currentPage == index ? 20 : 6,
+                            decoration: BoxDecoration(
+                              color: currentPage == index
+                                  ? const Color(0xFFFF7643)
+                                  : const Color(0xFFD8D8D8),
+                              borderRadius: BorderRadius.circular(3),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Spacer(flex: 3),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, SignInScreen.routeName);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          backgroundColor: const Color(0xFFFF7643),
+                          foregroundColor: Colors.white,
+                          minimumSize: const Size(double.infinity, 48),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                        ),
+                        child: const Text("Continue"),
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SplashContent extends StatefulWidget {
+  const SplashContent({
+    super.key,
+    this.text,
+    this.image,
+  });
+  final String? text, image;
+
+  @override
+  State<SplashContent> createState() => _SplashContentState();
+}
+
+class _SplashContentState extends State<SplashContent> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        const Spacer(),
+        const Text(
+          "WELCOME",
+          style: TextStyle(
+            fontSize: 32,
+            color: Color(0xFFFF7643),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          widget.text!,
+          textAlign: TextAlign.center,
+        ),
+        const Spacer(flex: 2),
+        Image.network(
+          widget.image!,
+          fit: BoxFit.contain,
+          height: 265,
+          width: 235,
+        ),
+      ],
+    );
+  }
+}
