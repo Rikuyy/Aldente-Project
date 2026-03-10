5ab8dacd1c7058e470ebbe1e96905008614aa92c
Nabeel D <enzydndraa@gmail.com>
2026-03-10 12:59:47 +0700

    first commit: frontend login, register dan dashboard

--- Laravel/resources/views/components/text-input.blade.php
+++ Laravel/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-200 focus:border-[#E91E63] focus:ring-[#E91E63] rounded-xl shadow-sm w-full py-3 px-4 transition duration-150']) !!}>
